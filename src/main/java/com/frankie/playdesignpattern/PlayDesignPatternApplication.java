badb6b848f75f779f6cf25c608825216f898654c
yycer <15900617310@163.com>
2020-03-31 22:05:55 +0800

    Create play-design-pattern project.

---
+++ src/main/java/com/frankie/playdesignpattern/PlayDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.frankie.playdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayDesignPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frankie/playdesignpattern/PlayDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankie.playdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
